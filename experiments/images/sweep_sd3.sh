765b3d93f214900d509a8fc6875e50bea09913c9
marcellobullo <bullo.marcello@gmail.com>
2026-09-11 15:21:11 +0000

    fix merge

--- experiments/images/sweep_sd3.sh
+++ experiments/images/sweep_sd3.sh
@@ -44,14 +44,8 @@ CACHE_ENCODED_PROMPTS="${CACHE_ENCODED_PROMPTS-$REPO/results/sd3/_prompt_cache}"
 #MIN_FREE_MIB="${MIN_FREE_MIB:-24000}"
 MIN_FREE_MIB="${MIN_FREE_MIB:-10000}"
 
-<<<<<<< HEAD
 CONFIGS="${CONFIGS-2,2 3,2 4,2 5,2 6,2 7,2 8,2 9,2 10,2 2,3 3,3 4,3 5,3 6,3 7,3 8,3 9,3 10,3}"
 RULES="${RULES-d-grs rmc paws}"
-=======
-# CONFIGS="${CONFIGS-2,2 3,2 4,2 5,2 6,2 7,2 8,2 9,2 10,2 2,3 3,3 4,3 5,3 6,3 7,3 8,3 9,3 10,3}"
-CONFIGS="${CONFIGS-2,2 3,2 4,2 5,2 6,2 2,3 3,3 4,3 5,3 6,3}"
-RULES="${RULES-d-grs rmc}"
->>>>>>> e492ca5 (fix result plotting)
 MATCH="${MATCH:-verification}"
 # Sampler options, S_noise and the timestep shift: protocol that does not appear
 # in a cell's output path, so the guard in run_cell is the only thing stopping
